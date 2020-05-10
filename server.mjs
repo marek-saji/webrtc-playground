9c5c4c6da826ad7fc3f3a460ecc3a707b7fa75a4
Marek ‘saji’ Augustynowicz <marek.aug@gmail.com>
2020-05-10 13:28:40 +0200

    Sockets

--- server.mjs
+++ server.mjs
@@ -3,6 +3,7 @@ import { join as joinPath, dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 import nodeStatic from 'node-static';
+import socketIO from 'socket.io';
 
 
 const PORT = process.env.PORT || 5000;
@@ -10,11 +11,60 @@ const DIR = dirname(fileURLToPath(import.meta.url));
 const STATIC_DIR = joinPath(DIR, 'static');
 
 
+// Logging
+
+function log (context, { clientId, clientName, room }, ...argv)
+{
+    const humanReadableContext = [
+        context,
+        clientName || null,
+        room ? `in #${room}` : null,
+        `(${clientId})`,
+    ].filter((a) => a != null).join(' ');
+    const prettyContext = `[0;34m[${humanReadableContext}][0;0m`;
+    // eslint-disable-next-line no-console
+    console.log(prettyContext, ...argv);
+}
+
+const logServer = log.bind(null, 'SERVER');
+
+
+// Static server
+
 const fileServer = new (nodeStatic.Server)(STATIC_DIR);
-createServer((req, res) => {
+const app = createServer((req, res) => {
     req.addListener('end', () => {
         fileServer.serve(req, res);
     }).resume();
 }).listen(PORT);
 
-process.stdout.write(`Serving ${STATIC_DIR} on http://[::1]:${PORT}\n`);
+
+// Sockets
+
+const io = socketIO.listen(app);
+io.sockets.on('connection', (socket) => {
+    const clientId = socket.id;
+    let clientName;
+    let room;
+    const logClient = (...argv) => log('CLIENT', { clientId, clientName, room }, ...argv);
+
+    logServer({ clientId }, 'connected');
+
+    socket.on('disconnect', () => {
+        logServer({ clientId, clientName, room }, 'disconnected');
+        socket.broadcast.in(room).emit('leaves', { clientName });
+    });
+
+    socket.on('hello', (payload) => {
+        clientName = payload.clientName;
+        room = payload.room;
+        socket.join(room);
+        socket.emit('hello', { clientName, room });
+        socket.broadcast.in(room).emit('enters', { clientName });
+    });
+
+    socket.on('log', logClient);
+});
+
+
+logServer({}, 'Serving', STATIC_DIR, 'on', `http://[::1]:${PORT}`);
---
+++ static/index.mjs
@@ -0,0 +1,37 @@
+/* global io */
+/* eslint-disable no-console */
+
+const url = new URL(window.location);
+
+const clientName = url.searchParams.get('name');
+const room = url.searchParams.get('room');
+
+const socket = io.connect();
+window.socket = socket; // DEBUG
+
+socket.on('connect', () => {
+    console.group('Connected');
+    socket.emit('hello', { clientName, room });
+    socket.emit('log', 'Hello');
+});
+
+socket.on('hello', (payload) => {
+    console.log('Hello', payload);
+});
+
+socket.on('enters', (payload) => {
+    console.debug('Other client entered', payload);
+});
+
+socket.on('leaves', (payload) => {
+    console.debug('Other client left', payload);
+});
+
+socket.on('disconnect', () => {
+    console.log('Disconnected');
+    console.groupEnd('Connected');
+});
+
+socket.on('message', (...argv) => {
+    console.debug('Message', ...argv);
+});
